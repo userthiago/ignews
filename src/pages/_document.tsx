e6b1153976596a0af95a2ad15b42590702c14c47
Thiago <thiagosantosprofissional@gmail.com>
2021-09-04 21:16:21 -0300

    feat: Adicionando área de section da página home.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet"></link>
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer} >
+        <section className={styles.hero} >
+          <span>👏 Hey, welcome</span>
+          <h1>New about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications<br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <Image src="/images/avatar.svg" alt="Girl coding" height={520} width={334} />
+      </main>
     </>
   )
 }
